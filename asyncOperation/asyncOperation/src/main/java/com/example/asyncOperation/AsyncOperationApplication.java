0550719f729c4fb1936bef7af43a3448c5c87d68
Akshay Satish. Gaikwad <akshaygaik@cybage.com>
2023-01-19 22:30:08 +0530

    add project in main

---
+++ asyncOperation/asyncOperation/src/main/java/com/example/asyncOperation/AsyncOperationApplication.java
@@ -0,0 +1,13 @@
+package com.example.asyncOperation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsyncOperationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsyncOperationApplication.class, args);
+	}
+
+}
